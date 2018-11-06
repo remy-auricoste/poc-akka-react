9c09d198f9e13baecf81c6f6674fa893d9739c62
remy <remy.auricoste@gmail.com>
2018-11-06 15:06:21 +0100

    1st working app displaying numbers and launching requests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const wellStyles = { maxWidth: 400, margin: '10px auto' };
+const inputStyle = { margin: '0.5em' };
+
 class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.onMinChange = this.onMinChange.bind(this);
+    this.onMaxChange = this.onMaxChange.bind(this);
+    this.onLaunch = this.onLaunch.bind(this);
+
+    this.state = {
+      min: '',
+      max: ''
+    };
+  }
+
+  validate() {
+    try {
+      const { min, max } = this.state;
+      if (typeof min !== 'number') {
+        throw new Error('min should be a number');
+      }
+      if (typeof max !== 'number') {
+        throw new Error('max should be a number');
+      }
+      if (min > max) {
+        throw new Error('max should be greater than min');
+      }
+    } catch (err) {
+      this.setState({ error: err.message });
+    }
+  }
+
+  getIntValue(e) {
+    return parseInt(e.target.value);
+  }
+  onMinChange(e) {
+    this.setState({ min: this.getIntValue(e) });
+  }
+  onMaxChange(e) {
+    this.setState({ max: this.getIntValue(e) });
+  }
+  onLaunch(e) {
+    e.preventDefault();
+    this.validate();
+    const { min, max } = this.state;
+    fetch(`http://localhost:9000/filter?min=${min}&max=${max}`, {
+      method: 'POST',
+      mode: 'cors',
+      redirect: 'follow',
+      headers: new Headers({
+        'Content-Type': 'text/plain'
+      })
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="row">
+          <div className="col-6">
+            <form>
+              <div className="form-group">
+                <p>Searching for numbers that matches :</p>
+                <p>x % 2 == x % 3</p>
+                <p>Look for numbers between :</p>
+                <input
+                  type="number"
+                  value={this.state.min}
+                  style={inputStyle}
+                  placeholder="Enter minimum value"
+                  onChange={this.onMinChange}
+                />
+                <label> and </label>
+                <input
+                  type="number"
+                  value={this.state.max}
+                  style={inputStyle}
+                  placeholder="Enter maximum value"
+                  onChange={this.onMaxChange}
+                />
+                <div className="well" style={wellStyles}>
+                  <button
+                    className="btn btn-primary btn-lg btn-block"
+                    onClick={this.onLaunch}
+                  >
+                    Envoyer
+                  </button>
+                </div>
+                <div className="alert alert-danger" hidden={!this.state.error}>
+                  {this.state.error}
+                </div>
+              </div>
+            </form>
+          </div>
+          <div className="col-6">
+            <p>Numbers :</p>
+            <p>{window.state.numbers.join(', ')}</p>
+          </div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+window.state = {
+  numbers: []
+};
+
+const render = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+const stream = new EventSource('http://localhost:9000/events');
+stream.onmessage = function(event) {
+  const { data } = event;
+  if (!data) {
+    console.log('SSE ping');
+  } else {
+    const number = parseInt(data);
+    window.state.numbers.push(number);
+    render();
+  }
+};
